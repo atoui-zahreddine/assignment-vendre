3295fb2804b9da583d8ffb01b55dfcdfd1f2639f
zahreddine <zahreddine.atoui@issatso.u-sousse.tn>
2022-10-02 17:37:35 +0100

    fix: fixed image size issue

--- src/components/Employees/EmployeeCard.tsx
+++ src/components/Employees/EmployeeCard.tsx
@@ -9,7 +9,11 @@ interface Props {
 const EmployeeCard: FunctionComponent<Props> = ({ avatar, name, email }) => {
   return (
     <div className="flex flex-col items-center justify-around space-y-5 px-12 py-6 bg-gray-300 rounded-2xl">
-      <img className="rounded-full" src={avatar} alt={name} />
+      <img
+        className="w-[128px] h-[128px] rounded-full"
+        src={avatar}
+        alt={name}
+      />
       <p className="capitalize font-bold">{name}</p>
       <a href={`mailto:${email}`} className="capitalize text-gray-700">
         contact
